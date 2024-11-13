85b0dd41b4fcacf82c6351672f24c3e2b83a1e7d
Lucas Regalado <lucasregalado@hotmail.com>
2024-11-12 23:59:15 -0300

    change html lang | Initializing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es_UY">
       <body className={`${inter.className} antialiased`}>
         <TopMenu />
         {children}
