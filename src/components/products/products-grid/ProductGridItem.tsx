be34c5e5ca1ee5b396400bc2bfd6ebbfbbced1b4
Lucas Regalado <lucasregalado@hotmail.com>
2024-11-17 21:46:44 -0300

    Improvements on ProductGridItem | List of products

--- src/components/products/products-grid/ProductGridItem.tsx
+++ src/components/products/products-grid/ProductGridItem.tsx
@@ -11,20 +11,23 @@ export const ProductGridItem = ({ product }: Props) => {
   const image = product.ProductImage[0];
   return (
     <li className="flex flex-col">
-      <Link href={`/product/${product.slug}`}>
+      <Link href={`/product/${product.slug}`} className="overflow-hidden">
         <Image
-          className="w-full h-auto mb-1"
+          className="w-full h-auto mb-1 transition-transform duration-300 ease-in-out transform hover:scale-110"
           src={image.url}
           alt={image.alt}
           width={500}
           height={500}
         />
       </Link>
-      <Link className="text-xs mb-1 flex-1 flex items-center" href={`/product/${product.slug}`}>
+      <Link
+        className="text-xs mb-1 flex-1 flex items-center hover:text-forestGreen"
+        href={`/product/${product.slug}`}
+      >
         <h3>{product.title}</h3>
       </Link>
 
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center hover:text-forestGreen">
         <Link href={`/product/${product.slug}`}>{`$ ${product.price}`}</Link>
         <AddToCart productId={product.id} />
       </div>
