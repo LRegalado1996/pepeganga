aab58f81a4eadd20364ef8305f8c7e69ce664832
Lucas Regalado <lucasregalado@hotmail.com>
2024-11-20 00:08:23 -0300

    Import cart store | Product view

---
+++ src/store/cart/cart-store.ts
@@ -0,0 +1,88 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+import type { CartProduct } from "@/interfaces";
+
+interface State {
+  cart: CartProduct[];
+
+  getTotalNumber: () => number;
+  getSummaryInformation: () => {
+    subTotal: number;
+    tax: number;
+    total: number;
+    itemsInCart: number;
+  };
+
+  addProductTocart: (product: CartProduct) => void;
+  updateProductQuantity: (product: CartProduct, quantity: number) => void;
+  removeProduct: (product: CartProduct) => void;
+}
+
+export const useCartStore = create<State>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+
+      //methods
+      getTotalNumber: () => {
+        const { cart } = get();
+        return cart.reduce((total, item) => total + item.quantity, 0);
+      },
+
+      getSummaryInformation: () => {
+        const { cart } = get();
+        const subTotal = cart.reduce((subTotal, item) => subTotal + item.price * item.quantity, 0);
+        const tax = subTotal * 0.15;
+        const total = subTotal + tax;
+        const itemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
+
+        return { subTotal, tax, total, itemsInCart };
+      },
+
+      addProductTocart: (product: CartProduct) => {
+        const { cart } = get();
+        const productInCart = cart.some((item) => item.id === product.id);
+
+        if (!productInCart) {
+          set({ cart: [...cart, product] });
+          return;
+        }
+
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id) {
+            return { ...item, quantity: item.quantity + product.quantity };
+          }
+
+          return item;
+        });
+
+        set({ cart: updatedCartProducts });
+      },
+
+      updateProductQuantity: (product: CartProduct, quantity: number) => {
+        const { cart } = get();
+        const updatedCartProducts = cart
+          .map((item) => {
+            if (item.id === product.id) {
+              return { ...item, quantity };
+            }
+            return item;
+          })
+          .filter((item) => item.quantity > 0);
+
+        set({ cart: updatedCartProducts });
+      },
+
+      removeProduct: (product: CartProduct) => {
+        const { cart } = get();
+        const updatedCartProducts = cart.filter((item) => !(item.id === product.id));
+
+        set({ cart: updatedCartProducts });
+      },
+    }),
+    {
+      name: "shopping-cart",
+    }
+  )
+);
