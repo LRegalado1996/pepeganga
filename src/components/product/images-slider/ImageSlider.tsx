f92f39c5815ae077d29d875fd258fbacb964cf81
Lucas Regalado <lucasregalado@hotmail.com>
2024-11-19 22:51:55 -0300

    Use swiper library for product images | Product view

--- src/components/product/images-slider/ImageSlider.tsx
+++ src/components/product/images-slider/ImageSlider.tsx
@@ -1,5 +1,16 @@
-import { ProductImage } from "@prisma/client";
+"use client";
+
+import { FreeMode, Pagination, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
+import type { ProductImage } from "@prisma/client";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./ImageSlider.css";
 
 interface Props {
   images: ProductImage[];
@@ -7,12 +18,14 @@ interface Props {
 
 export const ImageSlider = ({ images }: Props) => {
   return (
-    <ul className="flex flex-col w-full">
-      {images.map((image) => (
-        <li className="w-full" key={image.id}>
-          <Image className="w-full" src={image.url} alt={image.alt} width={1000} height={1000} />
-        </li>
-      ))}
-    </ul>
+    <div className="w-full max-h-screen">
+      <Swiper pagination navigation modules={[FreeMode, Pagination, Navigation]}>
+        {images.map((image) => (
+          <SwiperSlide key={image.id}>
+            <Image src={image.url} alt={image.alt} width={1000} height={1000} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
