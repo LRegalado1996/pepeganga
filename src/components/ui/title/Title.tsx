540fc0b206739e923ce6aebb219bb5cb492a1dc3
Lucas Regalado <lucasregalado@hotmail.com>
2024-11-25 14:44:12 -0300

    Add className property on title component | List of products

--- src/components/ui/title/Title.tsx
+++ src/components/ui/title/Title.tsx
@@ -2,8 +2,13 @@ import { mainFont } from "@/config/fonts";
 
 interface Props {
   name: string;
+  className?: string;
 }
 
-export const Title = ({ name }: Props) => {
-  return <h2 className={`${mainFont.className} antialiased text-xl font-bold mb-5`}>{name}</h2>;
+export const Title = ({ name, className }: Props) => {
+  return (
+    <h2 className={`${mainFont.className} antialiased text-xl font-bold mb-5 ${className}`}>
+      {name}
+    </h2>
+  );
 };
