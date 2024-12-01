991730246aeeb96d29a644a6e7387d7f526d7c58
Lucas Regalado <lucasregalado@hotmail.com>
2024-12-01 20:44:44 -0300

    Improvements on ordenSummary

--- src/components/cart/order-summary/OrderSummary.tsx
+++ src/components/cart/order-summary/OrderSummary.tsx
@@ -8,7 +8,7 @@ export const OrderSummary = () => {
   const [loaded, setLoaded] = useState(false);
   const summaryInformation = useCartStore((state) => state.getSummaryInformation);
 
-  const { subTotal, tax, total, itemsInCart } = summaryInformation();
+  const { total, itemsInCart } = summaryInformation();
 
   useEffect(() => {
     setLoaded(true);
@@ -18,16 +18,8 @@ export const OrderSummary = () => {
 
   return (
     <div className="grid grid-cols-2">
-      <span>No. Productos</span>
-      <span className="text-right">
-        {itemsInCart === 1 ? "1 artículo" : `${itemsInCart} artículos`}
-      </span>
-
-      <span>Subtotal</span>
-      <span className="text-right">{currencyFormat(subTotal)}</span>
-
-      <span>I.V.A.</span>
-      <span className="text-right">{currencyFormat(tax)}</span>
+      <span>Cantidad de artículos:</span>
+      <span className="text-right">{itemsInCart}</span>
 
       <span className="mt-5 text-2xl">Total:</span>
       <span className="mt-5 text-2xl text-right">{currencyFormat(total)}</span>
